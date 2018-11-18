129ce5cfa4eff3604bb1b46e7ceba8601e0fd3a1
”Ozi <31310132+OziOcb@users.noreply.github.com>
2018-11-18 18:24:18 +0000

    Add basic style for the table in index.php file

--- index.php
+++ index.php
@@ -13,33 +13,44 @@ $result = mysqli_query($conn, "SELECT * FROM users ORDER BY id DESC"); // using 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Homepage</title>
+
+	<!-- Bootstrap CSS -->
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<!-- My CSS -->
 	<link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-<a href="add.html">Add New Data</a><br/><br/>
+	<div class="container">
+		<a class="badge badge-primary p-3 my-3" href="add.html">Add New Data</a>
 
-	<table width='80%' border=0>
+		<table class="table">
+			<thead class="thead-dark">
+				<tr>
+					<th scope="col">Firstname</th>
+					<th scope="col">Lastname</th>
+					<th scope="col">Email</th>
+					<th scope="col">Message</th>
+					<th scope="col">Update</th>
+				</tr>
+			</thead>
+			<tbody>
 
-	<tr bgcolor='#CCCCCC'>
-		<td>Firstname</td>
-		<td>Lastname</td>
-		<td>Email</td>
-		<td>Message</td>
-		<td>Update</td>
-	</tr>
+				<?php
+					while($res = mysqli_fetch_array($result)) {
+						echo "<tr>";
+						echo "<td>".$res['firstname']."</td>";
+						echo "<td>".$res['lastname']."</td>";
+						echo "<td>".$res['email']."</td>";
+						echo "<td>".$res['message']."</td>";
+						echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"includes/delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
+					}
+				?>
+			</tbody>
+		</table>
+	</div>
 
-
-	<?php
-		while($res = mysqli_fetch_array($result)) {
-			echo "<tr>";
-			echo "<td>".$res['firstname']."</td>";
-			echo "<td>".$res['lastname']."</td>";
-			echo "<td>".$res['email']."</td>";
-			echo "<td>".$res['message']."</td>";
-			echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"includes/delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
-		}
-	?>
-	</table>
+	<!-- Bootstrap JS & JQuery -->
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" integrity="sha384-pjaaA8dDz/5BgdFUPX6M/9SUZv4d12SUPF0axWc+VRZkx5xU3daN+lYb49+Ax+Tl" crossorigin="anonymous"></script>
 </body>
 </html>
