1cb7452343c9ccabee6b299f4b8d64966f7866b5
”Ozi <31310132+OziOcb@users.noreply.github.com>
2018-11-18 17:24:13 +0000

    Add email validation in the edit.php file

--- edit.php
+++ edit.php
@@ -1,5 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Edit Data</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<!--
+	Update aready passed values
+-->
 <?php
-	// including the database connection file
+	// Include the database connection file
 	include_once("config.php");
 
 	if(isset($_POST['update'])) {
@@ -10,7 +24,7 @@
 		$email = mysqli_real_escape_string($conn, $_POST['email']);
 		$msg = mysqli_real_escape_string($conn, $_POST['message']);
 
-		// checking empty fields
+		// Check empty fields
 		if(empty($firstname) || empty($lastname) || empty($email) || empty($msg)) {
 
 			if(empty($firstname)) {
@@ -29,21 +43,40 @@
 				echo "<p class='error'>Message field is empty.</p><br/>";
 			}
 
-		} else {
-			//updating the table
-			$result = mysqli_query($conn, "UPDATE users SET firstname='$firstname',lastname='$lastname',email='$email',message='$msg' WHERE id=$id");
+			// Link to the previous page
+			echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+			exit();
 
-			//redirectig to the display page (index.php)
-			header("Location: index.php");
+		} else {
+			// If all the input fields are filled (not empty)
+			// Check if the email is valid
+			if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+
+				echo "<p class='error'>Email address is invalid.</p><br/>";
+				// Link to the previous page
+				echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+				exit();
+
+			} else {
+
+				// Update the table
+				$result = mysqli_query($conn, "UPDATE users SET firstname='$firstname',lastname='$lastname',email='$email',message='$msg' WHERE id=$id");
+
+				// Redirect to the display page (index.php)
+				header("Location: index.php");
+			}
 		}
 	}
 ?>
 
+<!--
+	Show already passed values
+-->
 <?php
-	//getting id from url
+	// Get id from url
 	$id = $_GET['id'];
 
-	//selecting data associated with this particular id
+	// Select data associated with this particular id
 	$result = mysqli_query($conn, "SELECT * FROM users WHERE id=$id");
 
 	while($res = mysqli_fetch_array($result)) {
@@ -54,18 +87,9 @@
 	}
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Edit Data</title>
-	<link rel="stylesheet" href="style.css">
-</head>
-
-<body>
+<!--
+	HTML code
+-->
 	<a href="index.php">Home</a>
 	<br/><br/>
 
@@ -85,7 +109,7 @@
 			</tr>
 			<tr>
 				<td>Message</td>
-				<td><textarea type="text" name="message"><?php echo $msg;?></textarea></td>
+				<td><textarea type="text" name="message" rows="10" cols="50"><?php echo $msg;?></textarea></td>
 			</tr>
 			<tr>
 				<td><input type="hidden" name="id" value="<?php echo $_GET['id'];?>"></td>
