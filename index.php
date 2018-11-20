1cf744bbd71b25d5532e73be635cd7b24443b93b
”Ozi <31310132+OziOcb@users.noreply.github.com>
2018-11-20 00:00:06 +0000

    Remove some Bootstrap classes

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-//including the database connection file
-include_once("includes/dbh.inc.php");
+	//including the database connection file
+	include_once("includes/dbh.inc.php");
 
-//fetching data in descending order (lastest entry first)
-$result = mysqli_query($conn, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
+	//fetching data in descending order (lastest entry first)
+	$result = mysqli_query($conn, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
 ?>
 
 <!DOCTYPE html>
@@ -23,15 +23,17 @@ $result = mysqli_query($conn, "SELECT * FROM users ORDER BY id DESC"); // using 
 <body>
 	<div class="container p-0 p-sm-2 bg-light">
 
-		<!-- Header -->
-		<header class="jumbotron text-light text-center rounded-0">
-			<h1 class="display-3 mb-3">Junior PHP Developer Task</h1>
-			<p class="lead">MySQL Display</p>
-			<p class="my-5">This page displays all submissions made to the MySQL database.</p>
-			<a class="header__btn btn btn-success p-4 btn-lg font-weight-bold border-0" href="add.html" role="button">Add New Data</a>
+		<!-- ──── Header ───────────────────────────────────────────────────────────────────────────── -->
+		<header class="header jumbotron">
+			<h1 class="header__title">Junior PHP Developer Task</h1>
+			<div class="header__desc">
+				<p>MySQL Display</p>
+				<p>This page displays all submissions made to the MySQL database.</p>
+			</div>
+			<a class="button button--primary btn" href="add.html" role="button">Add New Data</a>
 		</header>
 
-		<!-- Table -->
+		<!-- ──── Table ───────────────────────────────────────────────────────────────────────────── -->
 		<section class="table-responsive">
 			<table class="table table-striped table-hover">
 				<thead class="thead-dark">
@@ -45,6 +47,7 @@ $result = mysqli_query($conn, "SELECT * FROM users ORDER BY id DESC"); // using 
 				</thead>
 				<tbody>
 
+					<!-- PHP code that displays values from the databese -->
 					<?php
 						while($res = mysqli_fetch_array($result)) {
 							echo "<tr>";
@@ -59,8 +62,5 @@ $result = mysqli_query($conn, "SELECT * FROM users ORDER BY id DESC"); // using 
 			</table>
 		</section>
 	</div>
-
-	<!-- Bootstrap JS & JQuery -->
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" integrity="sha384-pjaaA8dDz/5BgdFUPX6M/9SUZv4d12SUPF0axWc+VRZkx5xU3daN+lYb49+Ax+Tl" crossorigin="anonymous"></script>
 </body>
 </html>
