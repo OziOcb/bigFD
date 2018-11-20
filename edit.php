08d223bc960a988be25d25a8df0a4a6dc7aa3bcd
”Ozi <31310132+OziOcb@users.noreply.github.com>
2018-11-20 00:24:38 +0000

    Change the table in the form in edit.php into bootstrap form

--- edit.php
+++ edit.php
@@ -5,117 +5,154 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Edit Data</title>
+
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+	<!-- Bootstrap CSS -->
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<!-- My CSS -->
 	<link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-<!--
-	Update aready passed values
--->
-<?php
-	// Include the database connection file
-	include_once("includes/dbh.inc.php");
+	<!-- Update aready passed value -->
+	<?php
+		// Include the database connection file
+		include_once("includes/dbh.inc.php");
 
-	if(isset($_POST['update'])) {
+		if(isset($_POST['update'])) {
 
-		$id = mysqli_real_escape_string($conn, $_POST['id']);
-		$firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
-		$lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
-		$email = mysqli_real_escape_string($conn, $_POST['email']);
-		$msg = mysqli_real_escape_string($conn, $_POST['message']);
+			$id = mysqli_real_escape_string($conn, $_POST['id']);
+			$firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
+			$lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
+			$email = mysqli_real_escape_string($conn, $_POST['email']);
+			$msg = mysqli_real_escape_string($conn, $_POST['message']);
 
-		// Check empty fields
-		if(empty($firstname) || empty($lastname) || empty($email) || empty($msg)) {
+			// Check empty fields
+			if(empty($firstname) || empty($lastname) || empty($email) || empty($msg)) {
 
-			if(empty($firstname)) {
-				echo "<p class='error'>Name field is empty.</p><br/>";
-			}
+				if(empty($firstname)) {
+					echo "<p class='error'>Name field is empty.</p><br/>";
+				}
 
-			if(empty($lastname)) {
-				echo "<p class='error'>Age field is empty.</p><br/>";
-			}
+				if(empty($lastname)) {
+					echo "<p class='error'>Age field is empty.</p><br/>";
+				}
 
-			if(empty($email)) {
-				echo "<p class='error'>Email field is empty.</p><br/>";
-			}
+				if(empty($email)) {
+					echo "<p class='error'>Email field is empty.</p><br/>";
+				}
 
-			if(empty($msg)) {
-				echo "<p class='error'>Message field is empty.</p><br/>";
-			}
+				if(empty($msg)) {
+					echo "<p class='error'>Message field is empty.</p><br/>";
+				}
 
-			// Link to the previous page
-			echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
-			exit();
-
-		} else {
-			// If all the input fields are filled (not empty)
-			// Check if the email is valid
-			if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-
-				echo "<p class='error'>Email address is invalid.</p><br/>";
 				// Link to the previous page
 				echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
 				exit();
 
 			} else {
+				// If all the input fields are filled (not empty)
+				// Check if the email is valid
+				if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
 
-				// Update the table
-				$result = mysqli_query($conn, "UPDATE users SET firstname='$firstname',lastname='$lastname',email='$email',message='$msg' WHERE id=$id");
+					echo "<p class='error'>Email address is invalid.</p><br/>";
+					// Link to the previous page
+					echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+					exit();
 
-				// Redirect to the display page (index.php)
-				header("Location: index.php");
+				} else {
+
+					// Update the table
+					$result = mysqli_query($conn, "UPDATE users SET firstname='$firstname',lastname='$lastname',email='$email',message='$msg' WHERE id=$id");
+
+					// Redirect to the display page (index.php)
+					header("Location: index.php");
+				}
 			}
 		}
-	}
-?>
+	?>
 
-<!--
-	Show already passed values
--->
-<?php
-	// Get id from url
-	$id = $_GET['id'];
+	<!-- Display already passed values -->
+	<?php
+		// Get id from url
+		$id = $_GET['id'];
 
-	// Select data associated with this particular id
-	$result = mysqli_query($conn, "SELECT * FROM users WHERE id=$id");
+		// Select data associated with this particular id
+		$result = mysqli_query($conn, "SELECT * FROM users WHERE id=$id");
 
-	while($res = mysqli_fetch_array($result)) {
-		$firstname = $res['firstname'];
-		$lastname = $res['lastname'];
-		$email = $res['email'];
-		$msg = $res['message'];
-	}
-?>
+		while($res = mysqli_fetch_array($result)) {
+			$firstname = $res['firstname'];
+			$lastname = $res['lastname'];
+			$email = $res['email'];
+			$msg = $res['message'];
+		}
+	?>
 
-<!--
-	HTML code
--->
-	<a href="index.php">Home</a>
-	<br/><br/>
+	<!-- ──── HTML ───────────────────────────────────────────────────────────────────────────── -->
+	<div class="container p-0 p-sm-2 bg-light">
 
-	<form action="edit.php" method="post" name="form1" >
-		<table border="0">
-			<tr>
-				<td>Firstname</td>
-				<td><input type="text" name="firstname" value="<?php echo $firstname;?>"></td>
-			</tr>
-			<tr>
-				<td>Lastname</td>
-				<td><input type="text" name="lastname" value="<?php echo $lastname;?>"></td>
-			</tr>
-			<tr>
-				<td>Email</td>
-				<td><input type="text" name="email" value="<?php echo $email;?>"></td>
-			</tr>
-			<tr>
-				<td>Message</td>
-				<td><textarea type="text" name="message" rows="10" cols="50"><?php echo $msg;?></textarea></td>
-			</tr>
-			<tr>
-				<td><input type="hidden" name="id" value="<?php echo $_GET['id'];?>"></td>
-				<td><button type="submit" name="update">Update</button></td>
-			</tr>
-		</table>
-	</form>
+		<!-- ──── Header ───────────────────────────────────────────────────────────────────────────── -->
+		<header class="header jumbotron">
+			<h1 class="header__title">Edit Data</h1>
+			<a class="button button--danger btn" href="index.php">Cancel</a>
+		</header>
+
+		<!-- ──── Form ───────────────────────────────────────────────────────────────────────────── -->
+		<section class="wrapper">
+			<form class="form" action="edit.php" method="post" name="form1">
+
+				<!-- Firstname -->
+				<div class="form__group form-group">
+					<label class="sr-only" for="reg-form__firstname">Firstname</label>
+					<div class="input-group">
+						<div class="input-group-prepend">
+							<div class="input__group-text input-group-text">
+								<span class="fas fa-user"></span>
+							</div>
+						</div>
+						<input name="firstname" class="input__input form-control" type="text" id="reg-form__firstname" aria-describedby="user firstname" placeholder="Firstname" required="required" value="<?php echo $firstname;?>"/>
+					</div>
+				</div>
+
+				<!-- Lastname -->
+				<div class="form__group form-group">
+					<label class="sr-only" for="reg-form__lastname">Lastname</label>
+					<div class="input-group">
+						<div class="input-group-prepend">
+							<div class="input__group-text input-group-text">
+								<span class="fas fa-user"></span>
+							</div>
+						</div>
+						<input name="lastname" class="input__input form-control" type="text" id="reg-form__lastname" aria-describedby="user lastname" placeholder="Lastname" required="required" value="<?php echo $lastname;?>"/>
+					</div>
+				</div>
+
+				<!-- Email -->
+				<div class="form__group form-group">
+					<label class="sr-only" for="reg-form__email">Email</label>
+					<div class="input-group">
+						<div class="input-group-prepend">
+							<div class="input__group-text input-group-text">
+								<span class="fas fa-envelope"></span>
+							</div>
+						</div>
+						<input name="email" class="input__input form-control" type="email" id="reg-form__email" aria-describedby="user email" placeholder="Email" required="required" value="<?php echo $email;?>"/>
+					</div>
+				</div>
+
+				<!-- Message -->
+				<div class="form__group form-group">
+					<label class="sr-only" for="reg-form__email">Message</label>
+					<textarea name="message" class="input__input form-control" type="text" id="reg-form__email" aria-describedby="your message" placeholder="Your message" rows="6" required="required"/><?php echo $msg;?></textarea>
+				</div>
+
+				<!-- Submit Button -->
+				<input type="hidden" name="id" value="<?php echo $_GET['id'];?>"></td>
+				<button name="update" class="form__btn btn" type="submit">Update</button>
+
+			</form>
+		</section>
+	</div>
 </body>
 </html>
